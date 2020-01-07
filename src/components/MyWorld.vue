736539101869e4bc3a2bc1d7b3a94809461952de
mdb1710 <mdb1710@gmail.com>
2020-01-07 12:32:08 -0500

    first commit

---
+++ src/components/MyWorld.vue
@@ -0,0 +1,21 @@
+<template>
+   <div class='my-world'>
+       <h1>{{ msg }}</h1>
+       <p>Testing out Vue for Friday's meeting</p>
+   </div> 
+</template>
+
+<script>
+export default {
+  name: 'MyWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+  h3 {
+  margin: 40px 0 0;
+}
+</style>
