dd63413b044bc34d602fa7bee6ba503a66fbb58c
mdb1710 <mdb1710@gmail.com>
2020-01-07 21:37:14 -0500

    added to links vue

---
+++ src/components/Social.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="social-media">
+    <h2>You can follow CodeByMDB all around the Internet here:</h2>
+    <ul>
+      <li><a href="https://twitter.com/codebymdb">Twitter</a></li>
+      <li><a href="https://facebook.com/codebymdb">Facebook</a></li>
+      <li><a href="https://instagram.com/codebymdb">Instagram</a></li>
+      <li><a href="https://dev.to/codebymdb">DEV Profile</a></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Social"
+};
+</script>
--- src/views/Links.vue
+++ src/views/Links.vue
@@ -1,5 +1,16 @@
 <template>
   <div class="about">
     <h1>This is a links page</h1>
+    <Social />
   </div>
 </template>
+
+<script>
+import Social from "@/components/Social.vue";
+export default {
+  name: "links",
+  components: {
+    Social
+  }
+};
+</script>
