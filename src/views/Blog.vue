813b819aa76094f3975fd1039edf8bd1ad486265
mdb1710 <mdb1710@gmail.com>
2020-02-14 21:16:48 -0500

    added styling

--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -4,3 +4,20 @@
     <router-view />
   </div>
 </template>
+<style lang="scss" scoped>
+.blog {
+  max-width: 50vw;
+  margin: 10rem auto;
+  /deep/ {
+    h1 {
+      font-size: 3rem;
+      margin-bottom: 0.2rem;
+      color: #42b883;
+    }
+    h4 {
+      margin-bottom: 3rem;
+      color: #35495e;
+    }
+  }
+}
+</style>
